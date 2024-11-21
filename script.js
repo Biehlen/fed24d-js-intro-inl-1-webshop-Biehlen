3d8f3ac110cae7e5cb58ffd768c05992d2c61373
Anna Biehl <annabiehl@hotmail.com>
2024-11-21 09:42:15 +0100

    lagt till plus och minus knappar och fått dom att fungera

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const products = [
     {
+        id: 0,
         name: 'Choklad och körsbär',
         price: '13kr',
         rating: '4',
+        amount: 0,
         category: 'Choklad',
         img: {
             url: 'assets/Chocolate.jpeg',
@@ -12,9 +14,11 @@ const products = [
         },
     },
     {
+        id: 1,
         name: 'Äppelpaj',
         price: '15kr',
         rating: '4,5',
+        amount: 0,
         category: 'Fyllda munkar',
         img: {
             url: 'assets/Apple.jpeg',
@@ -24,9 +28,11 @@ const products = [
         },
     },
     {
+        id: 2,
         name: 'Blåbär',
         price: '13kr',
         rating: '4',
+        amount: 0,
         category: 'Frukt och bär',
         img: {
             url: 'assets/Blueberry.jpeg',
@@ -36,9 +42,11 @@ const products = [
         },
     },
     {
+        id: 3,
         name: 'Kanel',
         price: '13kr',
         rating: '4.5',
+        amount: 0,
         category: 'Klassiker',
         img: {
             url: 'assets/Cinnamon.jpeg',
@@ -48,9 +56,11 @@ const products = [
         },
     },
     {
+        id: 4,
         name: 'Citron',
         price: '13kr',
         rating: '4',
+        amount: 0,
         category: 'Frukt och bär',
         img: {
             url: 'assets/Lemon.jpeg',
@@ -60,9 +70,11 @@ const products = [
         },
     },
     {
+        id: 5,
         name: 'Lime',
         price: '13kr',
         rating: '3.5',
+        amount: 0,
         category: 'Frukt och bär',
         img: {
             url: 'assets/Lime.jpeg',
@@ -72,9 +84,11 @@ const products = [
         },
     },
     {
+        id: 6,
         name: 'Marshmellow',
         price: '14kr',
         rating: '4.5',
+        amount: 0,
         category: 'Klassiker',
         img: {
             url: 'assets/Marshmellow.jpeg',
@@ -84,9 +98,11 @@ const products = [
         },
     },
     {
+        id: 7,
         name: 'Nutella',
         price: '15kr',
         rating: '4.5',
+        amount: 0,
         category: 'Fyllda munkar, choklad',
         img: {
             url: 'assets/Nutella.jpeg',
@@ -96,9 +112,11 @@ const products = [
         },
     },
     {
+        id: 8,
         name: 'Pink sprinkle',
         price: '13kr',
         rating: '4',
+        amount: 0,
         category: 'Klassiker',
         img: {
             url: 'assets/Pink sprinkle.jpeg',
@@ -108,9 +126,11 @@ const products = [
         },
     },
     {
+        id: 9,
         name: 'Smores',
         price: '15kr',
         rating: '4.5',
+        amount: 0,
         category: 'Choklad',
         img: {
             url: 'assets/Smores.jpeg',
@@ -120,9 +140,11 @@ const products = [
         },
     },
     {
+        id: 10,
         name: 'Vanilj',
         price: '14kr',
         rating: '4',
+        amount: 0,
         category: 'Fyllda munkar',
         img: {
             url: 'assets/Vanilla.jpeg',
@@ -132,9 +154,11 @@ const products = [
         },
     },
     {
+        id: 11,
         name: 'Jordgubb',
         price: '13kr',
         rating: '4',
+        amount: 0,
         category: 'Frukt och bär',
         img: {
             url: 'assets/Strawberry.jpeg',
@@ -152,15 +176,92 @@ const products = [
 const productsListDiv = document.querySelector('#products-list');
 
 // ------------------------------------------------
-// ----------- PRINT PRODUCTS IN HTML -------------
+// -----------------------SHOW PRODUCTS IN CART----
 // ------------------------------------------------
-products.forEach(product => {
-    productsListDiv.innerHTML += `
-        <article class="product">
-            <h3>${product.name}</h3>
-            <p>${product.price} kr</p>
-            <p>Rating: ${product.rating}</p>
-            <img src="${product.img.url}" alt="${product.img.alt}">
-        </article>
-    `;
-});
+
+const cart = document.querySelector('#cart-summary');
+function updateAndPrintCart() {
+    const cartProducts = products.filter((product) => product.amount > 0);
+}
+
+// ------------------------------------------------
+// -------------------- PRINT PRODUCTS IN HTML-----
+// ------------------------------------------------
+
+function printProductsList() {
+    productsListDiv.innerHTML = '';
+
+    products.forEach(product => {
+        productsListDiv.innerHTML += `
+            <article class="product">
+                <img src="${product.img.url}" alt="${product.img.alt}">
+                <h3>${product.name}</h3>
+                <p>${product.price} kr</p>
+                <p>Rating: ${product.rating}</p>
+                <span>Amount: ${product.amount}</span>
+                <div>
+                    <button class="decrease" id="decrease-${product.id}">-</button>
+                    <input type="number" min="0" value="${product.amount}" id="input-${product.id}">
+                    <button class="increase" id="increase-${product.id}">+</button>
+                </div>
+            </article>
+        `;
+    });
+
+    const decreaseButtons = document.querySelectorAll('button.decrease');
+    const increaseButtons = document.querySelectorAll('button.increase');
+
+    decreaseButtons.forEach(button => {
+        button.addEventListener('click', decreaseProductCount);
+    });
+
+    
+    increaseButtons.forEach(button => {
+        button.addEventListener('click', increaseProductCount);
+    });
+
+    // TODO Gör samma för decrease button, vid klick minska antalet 
+}
+
+printProductsList();
+
+function decreaseProductCount(e) {
+    const productId = Number(e.target.id.replace('decrease-', ''));
+
+    const foundProductIndex = products.findIndex(product => product.id === productId);
+    console.log('found product at index', foundProductIndex);
+
+    if (products[productId].amount <= 0) {
+        products[productId].amount = 0;
+    } else {
+        products[productId].amount -= 1;
+    }
+
+    printProductsList();
+
+}
+
+function increaseProductCount(e) {
+    const productId = Number(e.target.id.replace('increase-', ''));
+    console.log('clicked on button with id', productId);
+    // Leta rätt på produkten i arrayen som har det id:et
+    const foundProductIndex = products.findIndex(product => product.id === productId);
+    console.log('found product at index', foundProductIndex);
+
+    // Meddelande till mig själv. Om produkten inte finns, skriv ut felmeddelande i consolen
+    if (foundProductIndex === -1) {
+        console.error('Det finns ingen sådan produkt i produktlistan! Kolla att id:t är korrekt')
+        return;
+    }
+
+    //Öka dess amout med -1
+    products[foundProductIndex].amount += 1;
+
+
+    // Skriv ut produklistan
+    // Alternativ 1
+    // document.querySelector(`#input-${productId}`).value = products[foundProductIndex].amount;
+
+    // Alternativ 2
+    printProductsList();
+}
